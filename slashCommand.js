671ed3311a66d0586a29e0153e2c2ccdf473b559
sweetyclem <clementine@himama.com>
2020-06-06 16:43:53 -0400

    Add deploy requirements

--- slashCommand.js
+++ slashCommand.js
@@ -1,9 +1,11 @@
 const bodyParser = require('body-parser');
 const subjects = require('./elements/subjects.json')
+const cors = require('cors')
 
 module.exports.listenForCommands = async function (app) {
   app.use(bodyParser.urlencoded({ extended: true }))
   app.use(bodyParser.json())
+  app.use(cors())
 
   app.post('/commands', (req, res) => {
     const { token, user_id, channel_id } = req.body
