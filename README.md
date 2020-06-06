a289c0f8b2a6635fb0c34bdd2326eedf75fc8fb1
sweetyclem <clementine@himama.com>
2020-06-06 14:03:18 -0400

    Respond to app mention

---
+++ README.md
@@ -0,0 +1,13 @@
+## Running the app locally
+
+Tutorial for this app:
+
+### Tunnel local ports
+` ./ngrok http PORT`
+
+The events listen on `/events`
+The interactions listen on `/interactions`
+The command listen on `/commands`
+
+### Run the app
+`npm run start`
--- events.js
+++ events.js
@@ -1,18 +1,37 @@
-
 const { createEventAdapter } = require('@slack/events-api')
 const slackSigningSecret = process.env.SLACK_SIGNING_SECRET
 const slackEvents = createEventAdapter(slackSigningSecret)
+const { WebClient } = require('@slack/web-api')
+const token = process.env.SLACK_BOT_TOKEN
+const web = new WebClient(token)
+const subjects = require('./subjects.json')
 
-module.exports.listenForEvents = function (app) {
+function listenForEvents(app) {
   app.use('/events', slackEvents.requestListener())
+
+  slackEvents.on('app_mention', (event) => {
+    console.log(`Received an app_mention event from user ${event.user} in channel ${event.channel}`)
+    respondToEvent(event.channel)
+  })
+
+  // All errors in listeners are caught here. If this weren't caught, the program would terminate.
+  slackEvents.on('error', (error) => {
+    console.log(`error: ${error}`)
+  })
 }
 
-slackEvents.on('app_mention', (event) => {
-  console.log(`Received an app_mention event from user ${event.user} in channel ${event.channel}`)
-  respondToEvent(event.text, event.channel)
-})
+async function respondToEvent(channelId) {
+  try {
+    await web.chat.postMessage({
+      channel: channelId,
+      text: '',
+      attachments: [subjects]
+    })
+    console.log('Message posted!')
+  } catch (error) {
+    console.log(error)
+  }
+}
 
-// All errors in listeners are caught here. If this weren't caught, the program would terminate.
-slackEvents.on('error', (error) => {
-  console.log(`error: ${error.name}`)
-})
+module.exports.listenForEvents = listenForEvents
+module.exports.respondToEvent = respondToEvent
