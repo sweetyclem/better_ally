d2bc0ef093a4b3a4ffe0224da743f011283a75c8
sweetyclem <clementine@himama.com>
2020-06-06 14:42:28 -0400

    Respond to select choice

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ const express = require('express')
 const port = process.env.PORT
 const app = express()
 const events = require('./events')
-
+const interactions = require('./interactions')
 
 events.listenForEvents(app)
+interactions.listenForInteractions(app)
 app.listen(port, function () {
   console.log(`Listening on ${port}`)
 })
---
+++ interactions.js
@@ -0,0 +1,53 @@
+const { createMessageAdapter } = require('@slack/interactive-messages')
+const slackSigningSecret = process.env.SLACK_SIGNING_SECRET
+const slackInteractions = createMessageAdapter(slackSigningSecret)
+const articleOrBookButton = require('./articleOrBookButton.json')
+
+module.exports.listenForInteractions = function (app) {
+  app.use('/interactions', slackInteractions.requestListener())
+}
+
+slackInteractions.action({ type: 'select' }, (payload, respond) => {
+  respondToSelectDropdown(payload, respond)
+})
+
+function respondToSelectDropdown(payload, respond) {
+  const selectedOption = payload.actions[0].selected_options[0].value
+
+  if (payload.callback_id == 'subjects') {
+    switch (selectedOption) {
+      case 'anti_racism':
+        text = 'You selected anti racism.'
+        callbackId = 'anti_racism_article_book'
+        respondWithArticleOrBookNoButton(text, callbackId, respond)
+        break
+      case 'anti_sexism':
+        text = 'You selected anti sexism.'
+        callbackId = 'anti_sexism_article_book'
+        respondWithArticleOrBookNoButton(text, callbackId, respond)
+        break
+      case 'lgbtq_allyship':
+        text = 'You selected LGBTQ Allyship.'
+        callbackId = 'lgbtq_allyship_article_book'
+        respondWithArticleOrBookNoButton(text, callbackId, respond)
+        break
+      case 'autism_allyship':
+        text = 'You selected autism allyship.'
+        callbackId = 'autism_allyship_article_book'
+        respondWithArticleOrBookNoButton(text, callbackId, respond)
+        break
+    }
+  }
+  // Return a replacement message
+  return { text: 'Processing...' }
+}
+
+function respondWithArticleOrBookNoButton(text, callbackId, respond) {
+  articleOrBookButton.callback_id = callbackId
+  articleOrBookButton.text = 'Do you prefer an article or a book?'
+  respond({
+    text: text,
+    attachments: [articleOrBookButton],
+    replace_original: true
+  })
+}
